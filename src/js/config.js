4fd8a48180d7693053c96564135ac63ac06dc652
ezemg <ezequielmatiasgr@gmail.com>
2023-04-27 11:17:22 -0300

    Initial commit

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = '6f6f89a4-6939-4dfe-a610-f9f4166150a3';
+export const MODAL_CLOSE_SEC = 2.5;
